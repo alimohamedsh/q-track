f96774a97f3d7df95cdcfa39543240519a6735df
اسمك_هنا <__>
2026-03-18 15:53:07 +0200

    Fix: implement FilamentUser to fix 403 forbidden

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,17 +11,26 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    // بنعرفها مرة واحدة فقط في بداية الكلاس
     protected $guarded = [];
 
     protected $hidden = [
         'password',
         'remember_token',
     ];
+
+    /**
+     * ميثود تحديد صلاحية دخول لوحة التحكم
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // هنخليها true حالياً عشان تكسر الـ 403 وتدخل تجرب
+        // بعد كدة تقدر تخليها: return $this->hasRole('admin');
+        return true;
+    }
 
     protected function casts(): array
     {
